9e6927ee4cd6eccb0959f26c0e738e67ffd4d7ca
Danick Triantis <danick@triantis.nl>
2020-03-22 11:05:57 +0100

    Completed Library Import log4j

---
+++ src/timesheets/logging/LoggingTest.java
@@ -0,0 +1,13 @@
+package timesheets.logging;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingTest {
+	private final static Logger logger = LogManager.getLogger(LoggingTest.class);
+
+	public LoggingTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
