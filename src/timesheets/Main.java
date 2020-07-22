19a2d6d1173a8d425658f625613ae945a1bb27c0
CaptainKills <danick@triantis.nl>
2020-07-22 11:27:55 +0200

    Added Program Icon
    Icon image itself not final

--- src/timesheets/Main.java
+++ src/timesheets/Main.java
@@ -1,5 +1,6 @@
 package timesheets;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -9,6 +10,7 @@ import timesheets.logging.Logger;
 
 public class Main {
 	private static final Logger logger = new Logger(Main.class.toString());
+	private static final ImageIcon img = new ImageIcon("icon.png");
 	
 	public static void main(String[] args) {
 		LogManager.initialise();
@@ -19,13 +21,14 @@ public class Main {
 				logger.info("Initialising Mainframe.");
 				MainFrame frame = new MainFrame();
 				
-				logger.info("Set frame parameters.");
+				logger.info("Setting Frame Parameters.");
 				frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 				frame.setSize(frame.frameWidth, frame.frameHeight);
 				frame.setResizable(true);
 				frame.pack();
 				frame.setLocationRelativeTo(null);
 				frame.setVisible(true);
+				frame.setIconImage(img.getImage());
 				logger.info("Initialisation of SwingUtilities Complete.");
 			}
 		});
