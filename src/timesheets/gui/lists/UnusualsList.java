5f62227e86cb62b33743eb16137904935f931bc2
CaptainKills <danick@triantis.nl>
2020-07-23 10:10:22 +0200

    Update UnusualsList.java

--- src/timesheets/gui/lists/UnusualsList.java
+++ src/timesheets/gui/lists/UnusualsList.java
@@ -2,7 +2,6 @@ package timesheets.gui.lists;
 
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
-import javax.swing.ScrollPaneConstants;
 
 import timesheets.gui.combobox.EmployeeBox;
 import timesheets.gui.textareas.scrollpanes.ScrollPane;
