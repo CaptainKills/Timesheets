0129e09e154119f2f4e1e6ce8bf9831f4662359d
CaptainKills <danick@triantis.nl>
2020-07-25 13:03:20 +0200

    Migration to SQLite

--- src/timesheets/DataHandler.java
+++ src/timesheets/DataHandler.java
@@ -24,72 +24,72 @@ import timesheets.logging.Logger;
 public class DataHandler {
 	private static final Logger logger = new Logger(DataHandler.class.toString());
 
-	private final Path path_EmployeeData = Paths.get("data" + File.separator + "EmployeeData.dat").toAbsolutePath();
-	private final Path path_TimeData = Paths.get("data" + File.separator + "TimeData.dat").toAbsolutePath();
+	//private final Path path_EmployeeData = Paths.get("data" + File.separator + "EmployeeData.dat").toAbsolutePath();
+	//private final Path path_TimeData = Paths.get("data" + File.separator + "TimeData.dat").toAbsolutePath();
 	private final Path path_Settings = Paths.get("data" + File.separator + "Settings.dat").toAbsolutePath();
-	private File file_EmployeeData = path_EmployeeData.toFile();
-	private File file_TimeData = path_TimeData.toFile();
+	//private File file_EmployeeData = path_EmployeeData.toFile();
+	//private File file_TimeData = path_TimeData.toFile();
 	private File file_Settings = path_Settings.toFile();
 
-	private static final String date_REGEX = "(?<=[@]|[,])[^=]+";
-	private static final String time_REGEX = "(?<=[=]|[|]).....";
+	//private static final String date_REGEX = "(?<=[@]|[,])[^=]+";
+	//private static final String time_REGEX = "(?<=[=]|[|]).....";
 
-	public static Map<Integer, Employee> EmployeeList = new HashMap<Integer, Employee>();
+//	public static Map<Integer, Employee> EmployeeList = new HashMap<Integer, Employee>();
 	public static Map<String, String> settings = new LinkedHashMap<String, String>();
 
 	private final String defaultSettings = "" + "fontsize=10\n" + "width=1000\n" + "height=360\n" + "" + "" + "" + ""
 			+ "";
 
-	private final String defaultEmployeeData = "12345/Admin/1/0.0/true";
-	private final String defaultTimeData = "12345@";
+	//private final String defaultEmployeeData = "12345/Admin/1/0.0/true";
+	//private final String defaultTimeData = "12345@";
 
-	public void loadDataFromFiles() {
-		checkFiles();
-
-		logger.info("Loading Data From Files.");
-		try (Scanner employeeReader = new Scanner(file_EmployeeData);
-				Scanner timeReader = new Scanner(file_TimeData);
-				BufferedReader settingsReader = new BufferedReader(new FileReader(file_Settings))) {
-			
-			logger.info("Loading Employee Data from File.");
-			while (employeeReader.hasNext() & timeReader.hasNext()) {
-				String employeeImport = employeeReader.nextLine();
-				String timeImport = timeReader.nextLine();
-				assignDataToEmployee(employeeImport, timeImport);
-			}
-
-			logger.info("Loading Settings from File.");
-			String newline;
-			while ((newline = settingsReader.readLine()) != null) {
-				extractSettings(newline);
-			}
-		} catch (IOException e) {
-			logger.error("COULD NOT LOAD DATA FROM FILES: " + e);
-		}
-
-		logger.info("Loading Data From Files Complete.");
-	}
+//	public void loadDataFromFiles() {
+//		checkFiles();
+//
+//		logger.info("Loading Data From Files.");
+//		try (Scanner employeeReader = new Scanner(file_EmployeeData);
+//				Scanner timeReader = new Scanner(file_TimeData);
+//				BufferedReader settingsReader = new BufferedReader(new FileReader(file_Settings))) {
+//			
+//			logger.info("Loading Employee Data from File.");
+//			while (employeeReader.hasNext() & timeReader.hasNext()) {
+//				String employeeImport = employeeReader.nextLine();
+//				String timeImport = timeReader.nextLine();
+//				assignDataToEmployee(employeeImport, timeImport);
+//			}
+//
+//			logger.info("Loading Settings from File.");
+//			String newline;
+//			while ((newline = settingsReader.readLine()) != null) {
+//				extractSettings(newline);
+//			}
+//		} catch (IOException e) {
+//			logger.error("COULD NOT LOAD DATA FROM FILES: " + e);
+//		}
+//
+//		logger.info("Loading Data From Files Complete.");
+//	}
 
 	private void checkFiles() {
-		if (!file_EmployeeData.exists()) {
-			logger.info("EmplopyeeData file does not exist.");
-			createFile(file_EmployeeData);
-		} else if (file_EmployeeData.length() == 0) {
-			logger.info("EmployeeData File is empty: Length=" + file_EmployeeData.length());
-			writeDefaultsToFile(file_EmployeeData);
-		} else {
-			logger.info("EmployeeData file exists.");
-		}
+//		if (!file_EmployeeData.exists()) {
+//			logger.info("EmplopyeeData file does not exist.");
+//			createFile(file_EmployeeData);
+//		} else if (file_EmployeeData.length() == 0) {
+//			logger.info("EmployeeData File is empty: Length=" + file_EmployeeData.length());
+//			writeDefaultsToFile(file_EmployeeData);
+//		} else {
+//			logger.info("EmployeeData file exists.");
+//		}
 		
-		if (!file_TimeData.exists()) {
-			logger.info("TimeData file does not exist.");
-			createFile(file_TimeData);
-		} else if (file_TimeData.length() == 0) {
-			logger.info("TimeData File is empty: Length=" + file_TimeData.length());
-			writeDefaultsToFile(file_TimeData);
-		} else {
-			logger.info("TimeData file exists.");
-		}
+//		if (!file_TimeData.exists()) {
+//			logger.info("TimeData file does not exist.");
+//			createFile(file_TimeData);
+//		} else if (file_TimeData.length() == 0) {
+//			logger.info("TimeData File is empty: Length=" + file_TimeData.length());
+//			writeDefaultsToFile(file_TimeData);
+//		} else {
+//			logger.info("TimeData file exists.");
+//		}
 		
 		if (!file_Settings.exists()) {
 			logger.info("Settings file does not exist.");
@@ -128,108 +128,111 @@ public class DataHandler {
 			if (file.getAbsolutePath() == path_Settings.toString()) {
 				logger.debug("Writing default settings to Settings file.");
 				writer.print(defaultSettings);
-			} else if (file.getAbsolutePath() == path_EmployeeData.toString()) {
-				logger.debug("Writing default employee data to Settings file.");
-				writer.print(defaultEmployeeData);
-			} else if (file.getAbsolutePath() == path_TimeData.toString()) {
-				logger.debug("Writing default settings to Settings file.");
-				writer.print(defaultTimeData);
 			}
 		} catch (IOException e){
 			logger.error("COULD NOT WRITE DEFAULTS TO FILE: " + e);
 		}
 	}
 
-	private void assignDataToEmployee(String employee, String time) {
-		String[] EmployeeData = employee.split("/");
-		int import_ID = Integer.parseInt(EmployeeData[0]);
-		String import_Name = EmployeeData[1];
-		int import_Age = Integer.parseInt(EmployeeData[2]);
-		double import_Salary = Double.parseDouble(EmployeeData[3]);
-		boolean import_Admin = Boolean.parseBoolean(EmployeeData[4]);
-		TreeMap<LocalDate, LocalTime[]> import_WorkedTime;
+//	private void assignDataToEmployee(String employee, String time) {
+//		String[] EmployeeData = employee.split("/");
+//		int import_ID = Integer.parseInt(EmployeeData[0]);
+//		String import_Name = EmployeeData[1];
+//		int import_Age = Integer.parseInt(EmployeeData[2]);
+//		double import_Salary = Double.parseDouble(EmployeeData[3]);
+//		boolean import_Admin = Boolean.parseBoolean(EmployeeData[4]);
+//		TreeMap<LocalDate, LocalTime[]> import_WorkedTime;
+//
+//		if (time.length() > 6) {
+//			import_WorkedTime = getSavedTimeMap(time);
+//		} else {
+//			import_WorkedTime = new TreeMap<LocalDate, LocalTime[]>();
+//		}
+//
+//		Employee dummy_employee = new Employee(import_ID, import_Name, import_Age, import_Salary, import_Admin,
+//				import_WorkedTime);
+//		EmployeeList.put(import_ID, dummy_employee);
+//		logger.info("Employee Loaded: " + dummy_employee.getID_String());
+//	}
 
-		if (time.length() > 6) {
-			import_WorkedTime = getSavedTimeMap(time);
-		} else {
-			import_WorkedTime = new TreeMap<LocalDate, LocalTime[]>();
-		}
+//	private TreeMap<LocalDate, LocalTime[]> getSavedTimeMap(String time) {
+//		LinkedList<String> dates = extractREGEX(date_REGEX, time);
+//		LinkedList<String> times = extractREGEX(time_REGEX, time);
+//
+//		LinkedList<LocalDate> datesList = new LinkedList<LocalDate>();
+//		for (int index = 0; index < dates.size(); index++) {
+//			datesList.add(LocalDate.parse(dates.get(index)));
+//		}
+//
+//		LinkedList<LocalTime> timeList = new LinkedList<LocalTime>();
+//		for (int index = 0; index < times.size(); index++) {
+//			timeList.add(LocalTime.parse(times.get(index)));
+//		}
+//
+//		TreeMap<LocalDate, LocalTime[]> shiftsMap = new TreeMap<LocalDate, LocalTime[]>();
+//		int index = 0;
+//		for (LocalDate date : datesList) {
+//			LocalTime[] workedHours = { timeList.get(4 * index), timeList.get((4 * index) + 1),
+//					timeList.get((4 * index) + 2), timeList.get((4 * index) + 3) };
+//			shiftsMap.put(date, workedHours);
+//			index++;
+//		}
+//		return shiftsMap;
+//	}
 
-		Employee dummy_employee = new Employee(import_ID, import_Name, import_Age, import_Salary, import_Admin,
-				import_WorkedTime);
-		EmployeeList.put(import_ID, dummy_employee);
-		logger.info("Employee Loaded: " + dummy_employee.getID_String());
-	}
+//	private LinkedList<String> extractREGEX(String regex, String inputString) {
+//		Pattern regexPattern = Pattern.compile(regex);
+//		Matcher regexMatcher = regexPattern.matcher(inputString);
+//
+//		LinkedList<String> groupList = new LinkedList<String>();
+//		while (regexMatcher.find()) {
+//			String currentGroup = regexMatcher.group();
+//			groupList.add(currentGroup);
+//		}
+//		return groupList;
+//	}
 
-	private TreeMap<LocalDate, LocalTime[]> getSavedTimeMap(String time) {
-		LinkedList<String> dates = extractREGEX(date_REGEX, time);
-		LinkedList<String> times = extractREGEX(time_REGEX, time);
-
-		LinkedList<LocalDate> datesList = new LinkedList<LocalDate>();
-		for (int index = 0; index < dates.size(); index++) {
-			datesList.add(LocalDate.parse(dates.get(index)));
-		}
-
-		LinkedList<LocalTime> timeList = new LinkedList<LocalTime>();
-		for (int index = 0; index < times.size(); index++) {
-			timeList.add(LocalTime.parse(times.get(index)));
-		}
-
-		TreeMap<LocalDate, LocalTime[]> shiftsMap = new TreeMap<LocalDate, LocalTime[]>();
-		int index = 0;
-		for (LocalDate date : datesList) {
-			LocalTime[] workedHours = { timeList.get(4 * index), timeList.get((4 * index) + 1),
-					timeList.get((4 * index) + 2), timeList.get((4 * index) + 3) };
-			shiftsMap.put(date, workedHours);
-			index++;
-		}
-		return shiftsMap;
-	}
-
-	private LinkedList<String> extractREGEX(String regex, String inputString) {
-		Pattern regexPattern = Pattern.compile(regex);
-		Matcher regexMatcher = regexPattern.matcher(inputString);
-
-		LinkedList<String> groupList = new LinkedList<String>();
-		while (regexMatcher.find()) {
-			String currentGroup = regexMatcher.group();
-			groupList.add(currentGroup);
-		}
-		return groupList;
-	}
-
-	private void extractSettings(String line) {
-		String[] segments = line.split("=");
-		String setting = segments[0];
-		String value = segments[1];
-
-		settings.put(setting, value);
-		logger.info("Added Setting " + setting + ", Value: " + value);
-	}
-
-	public void saveDataToFiles() {
+	public void loadSettings() {
 		checkFiles();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(file_Settings))) {
+			String newline;
+			while ((newline = reader.readLine()) != null) {
+				String[] segments = newline.split("=");
+				String setting = segments[0];
+				String value = segments[1];
 
-		logger.info("Saving Data to Files.");
-		try (PrintWriter employeeWriter = new PrintWriter(file_EmployeeData);
-				PrintWriter timeWriter = new PrintWriter(file_TimeData);) {
-
-			writeDataToFile(employeeWriter, timeWriter);
-			logger.info("Succesfully saved data to files.");
+				settings.put(setting, value);
+				//logger.info("Added Setting " + setting + ", Value: " + value);
+			}
 		} catch (IOException e) {
-			logger.error("COULD NOT WRITE TO FILE: " + e);
+			e.printStackTrace();
 		}
 	}
 
-	private void writeDataToFile(PrintWriter employeeWriter, PrintWriter timeWriter) {
-		logger.debug("Writing employee data to file.");
+//	public void saveDataToFiles() {
+//		checkFiles();
+//
+//		logger.info("Saving Data to Files.");
+//		try (PrintWriter employeeWriter = new PrintWriter(file_EmployeeData);
+//				PrintWriter timeWriter = new PrintWriter(file_TimeData);) {
+//
+//			writeDataToFile(employeeWriter, timeWriter);
+//			logger.info("Succesfully saved data to files.");
+//		} catch (IOException e) {
+//			logger.error("COULD NOT WRITE TO FILE: " + e);
+//		}
+//	}
 
-		for (Map.Entry<Integer, Employee> entry : EmployeeList.entrySet()) {
-			Employee employee = entry.getValue();
-			employeeWriter.println(employee.toString());
-			timeWriter.println(employee.getID_String() + "@" + employee.timeMapToString());
-		}
-	}
+//	private void writeDataToFile(PrintWriter employeeWriter, PrintWriter timeWriter) {
+//		logger.debug("Writing employee data to file.");
+//
+//		for (Map.Entry<Integer, Employee> entry : EmployeeList.entrySet()) {
+//			Employee employee = entry.getValue();
+//			employeeWriter.println(employee.toString());
+//			timeWriter.println(employee.getID_String() + "@" + employee.timeMapToString());
+//		}
+//	}
 
 	public int generateNewID() {
 		int newID = new Random().nextInt(100000); // Generate number from 00000 to 99999 - a five digit ID
