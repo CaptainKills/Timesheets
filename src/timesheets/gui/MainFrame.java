5caa268683dca14e3281874f90174a41ed3aff2c
CaptainKills <danick@triantis.nl>
2020-07-26 11:26:16 +0200

    Fixed file mix-up

--- src/timesheets/gui/MainFrame.java
+++ src/timesheets/gui/MainFrame.java
@@ -21,7 +21,7 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		super("Timesheets");
 		logger.info("Start MainFrame Setup.");
-		
+
 		settings.loadSettings();
 		database.setupDatabase();
 		database.loadDatabase();
