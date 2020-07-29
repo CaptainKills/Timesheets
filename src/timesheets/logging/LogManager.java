7069c4bc7c617794cae270e2f114574c88107de2
CaptainKills <danick@triantis.nl>
2020-07-29 10:02:14 +0200

    Put ZipCompression aside
    Waiting to compress to zip since it is not working properly.

--- src/timesheets/logging/LogManager.java
+++ src/timesheets/logging/LogManager.java
@@ -1,12 +1,12 @@
 package timesheets.logging;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -22,12 +22,10 @@ public class LogManager {
 	private static String zipname = "Timesheets Log.zip";
 	private static String directoryName = "logs";
 
-	
 	private static final Path directory_path = Paths.get(directoryName).toAbsolutePath();
 	private static final Path log_path = Paths.get(directoryName + File.separator + filename).toAbsolutePath();
 	private static File directory_file = directory_path.toFile();
 	private static File log_file = log_path.toFile();
-	
 
 	private static boolean debugMode = false;
 
@@ -36,8 +34,8 @@ public class LogManager {
 			if (!log_file.getParentFile().exists()) {
 				log_file.getParentFile().mkdirs();
 			}
-			
-			archiveLogs();
+
+			//archiveLogs();
 			log_file.createNewFile();
 		} catch (IOException e) {
 			System.err.println("The file could not be created: " + e);
@@ -48,29 +46,45 @@ public class LogManager {
 	}
 
 	private static void archiveLogs() {
-		try (FileOutputStream fos = new FileOutputStream(directoryName + File.separator + zipname, true); ZipOutputStream zos = new ZipOutputStream(fos);) {
+		try (FileOutputStream fos = new FileOutputStream(directoryName + File.separator + zipname, true);
+				ZipOutputStream zos = new ZipOutputStream(fos);) {
 
 			String[] log_files = directory_file.list();
+			System.out.println("Starting Compression to Zip File.");
+			
 			for (String log : log_files) {
-				if(log == log_files[log_files.length-1]) {
-					return;
+				if (log == log_files[log_files.length - 1]) {
+					System.out.println("Compression complete.");
+					break;
 				}
-				
-				Path p = Paths.get(directoryName + File.separator + log);
-				File f = p.toFile();				
-				zos.putNextEntry(new ZipEntry(f.getName()));
 
-				byte[] bytes = Files.readAllBytes(p);
-				zos.write(bytes, 0, bytes.length);
-				zos.flush();
-				zos.closeEntry();
+				Path p = Paths.get(directoryName + File.separator + log);
+				File f = p.toFile();
+				System.out.println("Compressing " + p.toString());
+				zos.putNextEntry(new ZipEntry(f.getName()));
+				
+				try (FileInputStream fis = new FileInputStream(p.toString())){
+					byte[] buffer = new byte[1024];
+					
+					int length;
+					while((length = fis.read(buffer)) > 0) {
+						zos.write(buffer, 0, length);
+					}
+					
+					zos.flush();
+					zos.closeEntry();
+				}
+				System.out.println("Moving to next file.");
 			}
+
+			fos.flush();
+			zos.finish();
 		} catch (FileNotFoundException e) {
 			System.err.println("Could not find file Error: " + e);
 			e.printStackTrace();
-        } catch (IOException e) {
-        	System.err.println("I/O Error: " + e);
-        	e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("I/O Error: " + e);
+			e.printStackTrace();
 		}
 	}
 
