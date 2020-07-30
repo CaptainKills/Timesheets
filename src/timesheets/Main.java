1a93a06fbe86e537d081b55cbb429abddb079123
CaptainKills <danick@triantis.nl>
2020-07-30 12:07:54 +0200

    Added dedicated Resource package
    Added dedicated resource package and moved the icons there. Created ResourceHandler to handle all things Resources related (such as image icons).

--- src/timesheets/Main.java
+++ src/timesheets/Main.java
@@ -1,27 +1,15 @@
 package timesheets;
 
-import java.awt.Image;
-import java.util.LinkedList;
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 import timesheets.gui.MainFrame;
 import timesheets.logging.LogManager;
 import timesheets.logging.Logger;
+import timesheets.resources.ResourceHandler;
 
 public class Main {
 	private static final Logger logger = new Logger(Main.class.toString());
-	
-	private static final String resource_path = "timesheets/images/";
-	private static ClassLoader loader = Main.class.getClassLoader();
-	
-	private static final ImageIcon img16 = new ImageIcon(loader.getResource(resource_path + "atom16.png"));
-	private static final ImageIcon img32 = new ImageIcon(loader.getResource(resource_path + "atom32.png"));
-	private static final ImageIcon img128 = new ImageIcon(loader.getResource(resource_path + "atom128.png"));
-	private static final ImageIcon img256 = new ImageIcon(loader.getResource(resource_path + "atom256.png"));
-	private static final ImageIcon img512 = new ImageIcon(loader.getResource(resource_path + "atom512.png"));
 	
 	public static void main(String[] args) {
 		LogManager.initialise();
@@ -42,20 +30,9 @@ public class Main {
 				frame.pack();
 				frame.setLocationRelativeTo(null);
 				frame.setVisible(true);
-				frame.setIconImages(getIcons());
+				frame.setIconImages(ResourceHandler.getIcons());
 				logger.info("Initialisation of SwingUtilities Complete.");
 			}
 		});
-	}
-	
-	private static LinkedList<Image> getIcons(){
-		LinkedList<Image> list = new LinkedList<Image>();
-		list.add(img16.getImage());
-		list.add(img32.getImage());
-		list.add(img128.getImage());
-		list.add(img256.getImage());
-		list.add(img512.getImage());
-		
-		return list;
 	}
 }
---
+++ src/timesheets/resources/ResourceHandler.java
@@ -0,0 +1,29 @@
+package timesheets.resources;
+
+import java.awt.Image;
+import java.util.LinkedList;
+
+import javax.swing.ImageIcon;
+
+public class ResourceHandler {
+
+	private static final String images_path = "timesheets/resources/images/";
+	private static ClassLoader loader = ResourceHandler.class.getClassLoader();
+	
+	private static final ImageIcon img16 = new ImageIcon(loader.getResource(images_path + "atom16.png"));
+	private static final ImageIcon img32 = new ImageIcon(loader.getResource(images_path + "atom32.png"));
+	private static final ImageIcon img128 = new ImageIcon(loader.getResource(images_path + "atom128.png"));
+	private static final ImageIcon img256 = new ImageIcon(loader.getResource(images_path + "atom256.png"));
+	private static final ImageIcon img512 = new ImageIcon(loader.getResource(images_path + "atom512.png"));
+	
+	public static LinkedList<Image> getIcons(){
+		LinkedList<Image> list = new LinkedList<Image>();
+		list.add(img16.getImage());
+		list.add(img32.getImage());
+		list.add(img128.getImage());
+		list.add(img256.getImage());
+		list.add(img512.getImage());
+		
+		return list;
+	}
+}
