5f11aa005cdc29de9e154d871f091d5513d4c9f6
CaptainKills <danick@triantis.nl>
2020-08-10 10:43:32 +0200

    Update BackupBox.java

--- src/timesheets/gui/combobox/BackupBox.java
+++ src/timesheets/gui/combobox/BackupBox.java
@@ -10,10 +10,10 @@ import timesheets.gui.lists.FontList;
 import timesheets.logging.Logger;
 import timesheets.resources.ResourceHandler;
 
-public class BackupBox<T> extends JComboBox<T>{
+public class BackupBox<T> extends JComboBox<T> {
 	private static final long serialVersionUID = -229433229070533167L;
 	private static final Logger logger = new Logger(BackupBox.class.toString());
-	
+
 	private static String[] directory_files = ResourceHandler.data_directory_path.toFile().list();
 
 	public BackupBox() {
@@ -21,13 +21,13 @@ public class BackupBox<T> extends JComboBox<T>{
 		setFont(FontList.normalFont);
 		setVisible(true);
 		setEnabled(true);
-		
+
 		addItemListener(new ItemListener() {
 			@Override
 			public void itemStateChanged(ItemEvent event) {
 				if (getSelectedItem() != null) {
-					for(String f : directory_files) {
-						if(f.equals(getSelectedItem().toString())) {
+					for (String f : directory_files) {
+						if (f.equals(getSelectedItem().toString())) {
 							logger.debug(f + " got selected.");
 						}
 					}
@@ -37,20 +37,19 @@ public class BackupBox<T> extends JComboBox<T>{
 				}
 			}
 		});
-		
+
 		logger.debug("BackupBox initialised.");
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void loadBackupsInBox() {
 		removeAllItems();
 		for (String backup : directory_files) {
-			if(backup.contains("Backup")) {
-				addItem((T) backup.substring(0, backup.length()-10));
+			if (backup.contains("Backup")) {
+				addItem((T) backup.substring(0, backup.length() - 10));
 			}
 		}
 		setSelectedItem(-1);
-
 	}
 
 }
