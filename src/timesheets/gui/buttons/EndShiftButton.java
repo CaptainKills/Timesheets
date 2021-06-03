bbce11f7db823891ae2a308cff5197160dd2c8fb
Danick Triantis <danick@triantis.nl>
2021-06-03 18:03:26 +0200

    Redesigned Shift Storing Code
    Code is now much clearer, and actually works as supposed to.

--- src/timesheets/gui/buttons/EndShiftButton.java
+++ src/timesheets/gui/buttons/EndShiftButton.java
@@ -23,10 +23,8 @@ public class EndShiftButton extends JButton {
 
 	private Database database = new Database();
 	private TimeHandler time = new TimeHandler();
-
-	private LocalTime[] previousShift, newShift;
-	private LocalTime currentTime, differenceTime, additionalBreakTime, newTime;
-	private LocalDate currentDate;
+	
+	private LocalTime[] newShift;
 
 	public EndShiftButton() {
 		super("End Shift");
@@ -40,34 +38,51 @@ public class EndShiftButton extends JButton {
 				int id = Integer.parseInt(TextFieldList.inputField.getText());
 				Employee activeEmployee = Database.EmployeeList.get(id);
 
-				currentDate = time.getCurrentDate();
-				currentTime = time.roundOffTime(time.getCurrentTime());
+				LocalDate currentDate = time.getCurrentDate();
+				LocalTime currentTime = time.roundOffTime(time.getCurrentTime());
 				activeEmployee.setTimeEnded(currentTime);
 
-				differenceTime = time.calculateDifference(activeEmployee.getTimeStarted(),
+				LocalTime newWorkedTime = time.calculateDifference(activeEmployee.getTimeStarted(),
 						activeEmployee.getTimeEnded(), activeEmployee.getTimePaused());
 
 				if (activeEmployee.getTimePaused() != null) {
-					DisplayList.loginTextArea.updateInfoText("Total time worked this shift: " + differenceTime
-							+ ", and " + activeEmployee.getTimePaused() + " of breaktime.");
+					DisplayList.loginTextArea.updateInfoText("Total time worked this shift: " + newWorkedTime + ", and " + activeEmployee.getTimePaused() + " of breaktime.");
 				} else if (activeEmployee.getTimePaused() == null) {
-					DisplayList.loginTextArea.updateInfoText("Total time worked this shift: " + differenceTime + ", without a break.");
+					DisplayList.loginTextArea.updateInfoText("Total time worked this shift: " + newWorkedTime + ".");
 				}
+				
 				logger.info("Employee " + activeEmployee.getID_String() + " ended their shift at: " + currentTime);
+				
+				
 
 				if (activeEmployee.getWorkedTime().containsKey(currentDate)) {
-					previousShift = activeEmployee.getWorkedTime().get(currentDate);
-					newTime = time.addUp(previousShift[3], differenceTime);
+					logger.info("Employee already worked on this date.");
+					LocalTime[] previousShift = activeEmployee.getWorkedTime().get(currentDate);
+					LocalTime prevStartTime = previousShift[0];
+					LocalTime prevEndTime = previousShift[1];
+					LocalTime prevBreakTime = previousShift[2];
+					LocalTime prevTotalTime = previousShift[3];
+					
+					LocalTime newTotalTime = time.addUp(prevTotalTime, newWorkedTime);
+					logger.info("Prev Total Time: " + prevTotalTime + ", Next Total Time: " + newWorkedTime + ", New Total Time: " + newTotalTime);
 
-					additionalBreakTime = time.calculateDifference(previousShift[1], activeEmployee.getTimeStarted());
-					activeEmployee.setTimePaused(time.addUp(previousShift[2], additionalBreakTime, activeEmployee.getTimePaused()));
+					LocalTime gapBetweenShifts = time.calculateDifference(prevEndTime, activeEmployee.getTimeStarted());
+					logger.info("Prev End Time: " + prevEndTime + ", Current Start Time: " + activeEmployee.getTimeStarted() + ", Gap Inbetween: " + gapBetweenShifts);
+					
+					LocalTime totalBreakTime = time.addUp(prevBreakTime, activeEmployee.getTimePaused());
+					logger.info("Prev Break Time: " + prevBreakTime + ", New Break Time: " + activeEmployee.getTimePaused() + ", Total Break Time: " + totalBreakTime);
+					
+					LocalTime totalPausedTime = time.addUp(totalBreakTime, gapBetweenShifts);
+					logger.info("Total Break Time: " + totalBreakTime + ", Additional Gap Time: " + gapBetweenShifts + ", Total Paused Time: " + totalPausedTime);
 
-					addToShift(previousShift[0], activeEmployee.getTimeEnded(), activeEmployee.getTimePaused(), newTime);
+					newShift = formatShift(prevStartTime, activeEmployee.getTimeEnded(), totalPausedTime, newTotalTime);
 				} else {
-					newTime = differenceTime;
-					addToShift(activeEmployee.getTimeStarted(), activeEmployee.getTimeEnded(), activeEmployee.getTimePaused(), newTime);
+					logger.info("Employee has not worked on this date.");
+					newShift = formatShift(activeEmployee.getTimeStarted(), activeEmployee.getTimeEnded(), activeEmployee.getTimePaused(), newWorkedTime);
+					
 				}
-
+				
+				logger.info("New Shift: ST=" + newShift[0] + ", ET=" + newShift[1] + ", BT=" + newShift[2] + ", TT=" + newShift[3]);
 				activeEmployee.setWorkedTime(currentDate, newShift);
 				activeEmployee.resetTime();
 				database.insertTime(id, currentDate, newShift);
@@ -79,8 +94,8 @@ public class EndShiftButton extends JButton {
 		logger.debug("EndShiftButton initialised.");
 	}
 
-	private void addToShift(LocalTime start, LocalTime end, LocalTime paused, LocalTime difference) {
-		newShift = new LocalTime[4];
+	private LocalTime[] formatShift(LocalTime start, LocalTime end, LocalTime paused, LocalTime difference) {
+		LocalTime[] newShift = new LocalTime[4];
 		newShift[0] = start;
 		newShift[1] = end;
 
@@ -90,6 +105,8 @@ public class EndShiftButton extends JButton {
 			newShift[2] = paused;
 		}
 		newShift[3] = difference;
+		
+		return newShift;
 	}
 
 }
